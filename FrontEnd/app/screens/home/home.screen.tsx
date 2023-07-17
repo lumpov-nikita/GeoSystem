011c608c547efb502d9bc1c54ea0b140bd73e493
lumpov-nikita <iksrf@bk.ru>
2023-07-17 07:53:23 -0700

    Рисую квадраты

--- FrontEnd/app/screens/home/home.screen.tsx
+++ FrontEnd/app/screens/home/home.screen.tsx
@@ -1,175 +1,189 @@
 import React, { useState } from 'react';
-import { YMaps, Map, Polygon, Polyline } from '@pbe/react-yandex-maps';
-import { SafeAreaView, View, Modal, TouchableOpacity, Text } from 'react-native';
+import { YMaps, Map, Polygon, Placemark } from '@pbe/react-yandex-maps';
+import { SafeAreaView, TouchableOpacity, Text, View } from 'react-native';
 import { Title } from 'react-native-paper';
 import { homeStyle } from './home.style';
 
-const hexagonCoordinates = (center, radius) => {
-  const angle_deg = 15;
-  const angle_rad = Math.PI / 180 * angle_deg;
-  const coords = [];
-  
-  for (let i = 0; i < 24; i++) {
-    const x = center[0] + radius * Math.cos(angle_rad * i);
-    const y = center[1] + radius * Math.sin(angle_rad * i);
-    coords.push([x, y]);
-  }
-
-  return coords;
-}
-
-const squareCoordinates = (center, radius) => {
-  const topLeft = [center[0] - radius, center[1] + radius];
-  const topRight = [center[0] + radius, center[1] + radius];
-  const bottomRight = [center[0] + radius, center[1] - radius];
-  const bottomLeft = [center[0] - radius, center[1] - radius];
-  
-  return [topLeft, topRight, bottomRight, bottomLeft];
-}
-
 const HomeScreen = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalVideoVisible, setModalVideoVisible] = useState(false);
-  const [selectedPoint, setSelectedPoint] = useState({});
-  const [zoom, setZoom] = useState(10);
+  const [isSelectingArea, setIsSelectingArea] = useState(false);
+  const [selectedPoints, setSelectedPoints] = useState([]);
+  const [isSqueresVisible, setSqueresVisible] = useState(false);
+  const [selectedPointIndex, setSelectedPointIndex] = useState(0);
+  const [squares, setSquares] = useState([]);
+  const [hint, setHint] = useState(""); 
 
-  const handlePlacemarkClick = (e, name) => {
-    e.preventDefault();
-    setSelectedPoint({ name });
-    setModalVisible(true);
-  };
+  const handleSquareMouseEnter = (event, square) => {
+    const squareCoordinates = square.geometry.coordinates[0];
 
-  const handleModalClose = () => {
-    setModalVisible(false);
-  };
+    setHint(squareCoordinates.join(", ")); 
+  };  
 
-  const handleModalVideoClose = () => {
-    setModalVideoVisible(false);
-  };
-
-  const handleModalVideo = () => {
-    setModalVideoVisible(true);
-  };
-
-  const handleZoomChange = (e) => {
-    setZoom(20 - e.get('newZoom'));
-  };
-
-  const points = [
-    { coordinates: [54.738276, 20.489808], name: "Point A", color: "#0000FF"},
-    { coordinates: [54.753289, 20.516721], name: "Point B", color: "#ff0000" },
-    { coordinates: [54.726419, 20.502906], name: "Point C", color: "#ff0000" },
-    { coordinates: [54.743931, 20.458915], name: "Point D", color: "#0000FF" },
-    { coordinates: [54.709743, 20.500256], name: "Point E", color: "#0000FF" }
-  ];
-
-  const polylineCoordinates = points.map(point => point.coordinates);
-
-  const squares = points.map(({coordinates, color}, index) => {
-    const radius = 0.01 * Math.pow(2, zoom - 10);
-    const square = squareCoordinates(coordinates, radius);
-    return (
-      <Polygon 
-        key={index}
-        geometry={{
-          type: 'Polygon',
-          coordinates: [square]
-        }}
-        options={{
-          fillColor: color,
-          strokeColor: '#000000',
-          strokeWidth: 2,
-          strokeOpacity: 0.8
-        }}
-        onClick={(e) => handlePlacemarkClick(e, name)}
-      />
-    );
-  });
-
-
-  const hexagons = points.map(({coordinates, color}, index) => {
-    const hexagon = hexagonCoordinates(coordinates, 0.01);
-    return (
-      <Polygon 
-        key={index}
-        geometry={{
-          type: 'Polygon',
-          coordinates: [hexagon]
-        }}
-        options={{fillColor: color,
-        fillOpacity: 0.1
-        }}
-      />
-    );
-  });
+  const handleGenerateData = () => {
+    const points = selectedPoints.map(point => point.coordinates);
   
-return (
+    if (points.length !== 4) {
+      return;
+    }
+  
+    const minX = Math.min(...points.map(point => point[0]));
+    const minY = Math.min(...points.map(point => point[1]));
+    const maxX = Math.max(...points.map(point => point[0]));
+    const maxY = Math.max(...points.map(point => point[1]));
+  
+    const squares = [];
+  
+    for (let x = minX; x < maxX; x += 0.01) {
+      for (let y = minY; y < maxY; y += 0.01) {
+        squares.push([
+          [x, y],
+          [x + 0.01, y],
+          [x + 0.01, y + 0.01],
+          [x, y + 0.01],
+        ]);
+      }
+    }
+    
+    handleReset();
+    setSqueresVisible(true);
+    setSelectedPoints([]);
+    setSelectedPointIndex(0);
+    setSquaresOnMap(squares);
+  };
+  
+  const setSquaresOnMap = (squares) => {
+    const mapSquares = squares.map(square => ({
+      geometry: {
+        type: 'Polygon',
+        coordinates: [square],
+      },
+      options: {
+        fillColor: '#C4C3C4',
+        fillOpacity: 0.01,
+        strokeColor: '#C4C3C4',
+        strokeWidth: 1,
+      },
+    }));
+  
+    setSquares(mapSquares);
+  };  
+
+  const handleReset = () => {
+    setIsSelectingArea(false);
+    setSelectedPoints([]);
+    setSqueresVisible(false);
+    setSelectedPointIndex(0);
+  };
+  
+  const handleMapClick = (event) => {
+    if (isSelectingArea) {
+      const point = event.get('coords');
+      if (selectedPoints.length < 4) {
+        setSelectedPoints([
+          ...selectedPoints,
+          {
+            coordinates: point,
+            name: `Point ${selectedPoints.length + 1}`,
+            color: "#0000FF",
+          },
+        ]);
+        setSelectedPointIndex(selectedPoints.length + 1);
+      } else {
+        setSelectedPoints([]);
+        setSelectedPointIndex(0);
+      }
+    }    
+  };
+
+  const handleSelectArea = () => {
+    setIsSelectingArea(true);
+  };
+
+  return (
     <SafeAreaView style={homeStyle.page}>
-      <Title>Map</Title>
+       <View style={homeStyle.titleContainer}>
+        <Title style={homeStyle.title}>Map</Title>
+        <TouchableOpacity onPress={handleSelectArea} style={homeStyle.button}>
+        <Text style={homeStyle.buttonText}>Выбрать область</Text>
+      </TouchableOpacity>
+      </View>
+
+
       <YMaps 
         enterprise
         query={{
           lang: 'ru_RU',
           load: "package.full", 
           apikey: "af7c0f53-a625-45b6-9e95-fd90a65132des"
-        }}>
+        }}
+      >
         <Map
           style={homeStyle.container}
           defaultState={{ center: [54.738276, 20.489808], zoom: 10 }}
-          onBoundsChange={handleZoomChange}
-          onZoomChange={handleZoomChange}
           options={{
-            suppressMapOpenBlock: true,
+            suppressMapOpenBlock: true
           }}
           modules={['geoObject.addon.balloon']}
+          onClick={handleMapClick}
         >
-          {hexagons}
-          {squares}
-          <Polyline
-              geometry={polylineCoordinates}
+          {isSelectingArea && (
+            <View style={homeStyle.hint}>
+            {selectedPoints.length === 0 && (
+              <Text style={homeStyle.hintText}>Выберите первую точку</Text>
+            )}
+            {selectedPoints.length >= 1 && selectedPoints.length < 4 && (
+              <Text style={homeStyle.hintText}>Выберите следующую точку</Text>
+            )}
+            {selectedPoints.length === 4 && (
+              <>
+                <TouchableOpacity onPress={handleGenerateData} style={homeStyle.button}>
+                  <Text style={homeStyle.buttonText}>Сгенерируйте данные</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleReset} style={homeStyle.button}>
+                  <Text style={homeStyle.buttonText}>Сбросить</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+          )}
+          {isSqueresVisible && (
+            <View style={homeStyle.hint}>
+              <Text style={homeStyle.hintText}>{hint}</Text>
+          </View>
+          )}
+          <Polygon 
+            key='0000'
+            geometry={{
+              type: 'Polygon',
+              coordinates: [selectedPoints.map(point => point.coordinates)]
+            }}
+            options={{
+              fillColor: "#0000FF",
+              fillOpacity: 0.1
+            }}
+          />
+          {squares.map((square, index) => (
+            <Polygon 
+              key={index}
+              geometry={square.geometry}
+              options={square.options}
+              onMouseEnter={(event) => handleSquareMouseEnter(event, square)}
+            />
+          ))}
+          {selectedPoints.map((point, index) => (
+            <Placemark
+              key={index}
+              geometry={point.coordinates}
               options={{
-                strokeColor: '#000000',
-                strokeWidth: 3,
-                strokeOpacity: 0.5,
+                iconColor: point.color,
+              }}
+              properties={{
+                balloonContent: `Point ${index + 1}`,
               }}
             />
+          ))}
         </Map>
       </YMaps>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={handleModalClose}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <View style={{ backgroundColor: 'white', padding: 20 }}>
-            <Text style={{ textAlign: 'center' }}>Placemark {selectedPoint.name} clicked.</Text>
-            <TouchableOpacity style={{ marginTop: 20 }} onPress={handleModalVideo}>
-              <Text style={{ color: 'blue', textAlign: 'center' }}>Видеотрансляция</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{ marginTop: 20 }} onPress={handleModalClose}>
-              <Text style={{ color: 'blue', textAlign: 'center' }}>Открыть отчет</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{ marginTop: 20 }} onPress={handleModalClose}>
-              <Text style={{ color: 'blue', textAlign: 'center' }}>Закрыть</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVideoVisible}
-        onRequestClose={handleModalClose}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <View style={{ backgroundColor: 'white', padding: 20 }}>
-            <Text style={{ textAlign: 'center' }}>Placemark {selectedPoint.name} clicked.</Text>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/KG6SL6Mf7ak" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <TouchableOpacity style={{ marginTop: 20 }} onPress={handleModalVideoClose}>
-              <Text style={{ color: 'blue', textAlign: 'center' }}>Закрыть</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
+      
     </SafeAreaView>
   );
 };
--- FrontEnd/app/screens/home/home.style.ts
+++ FrontEnd/app/screens/home/home.style.ts
@@ -2,15 +2,73 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../../App.style";
 
 export const homeStyle = StyleSheet.create({
-    page: {
+  hintText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  hint: {
+    position: 'absolute',
+    top: 90,
+    right: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 10,
+    borderRadius: 5,
+  },
+  
+  page: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: theme.colors.primary
       },
-    container: {
-        height: "100%",
-        width: "100%",
-        flex: 1
-      }
+  container: {
+      height: "100%",
+      width: "100%",
+      flex: 1,
+      marginTop: -50
+    },
+    titleContainer: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      height: 60,
+      backgroundColor: theme.colors.primary,
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 1,
+    
+    },
+    title: {
+      color: 'white',
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  
+    button: {
+      backgroundColor: 'transparent',
+      maxWidth: 300,
+      width: '80%',
+      alignSelf: 'center',
+      borderRadius: 10,
+      borderWidth: 1, 
+      borderColor: 'white', 
+      marginBottom: 10,
+      paddingVertical: 5,
+    },
+    hintContainer: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'flex-end',
+    },
+    
 })
